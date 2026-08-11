70663bb3d7f08f6b1cd5f7d58702ab28205d0426
kkydoniatis <kkydonia@uwyo.edu>
2026-08-11 01:28:05 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -15,8 +15,44 @@ var ptx_lunr_docs = [
   "url": "sec-ch01-01-Vectors2DIntroduction.html",
   "type": "Section",
   "number": "1.1",
-  "title": "Introduction to Vectors in 2D",
-  "body": " Introduction to Vectors in 2D  Text of section.  "
+  "title": "Vectors in 2D",
+  "body": " Vectors in 2D   Vectors   A vector is an object defined by     its magnitude (length)    and its direction.     They are geometrically represented by rays\/arrows and symbolically with a lowercase letter with an arrow above it or in bold face (such as or or , etc.).    We will be drawing 2D vectors in the -plane (Cartesian plane). In this setting, we can define a vector by stating its starting point (arrow tail) and ending point (arrow head).  When a vector is given by its starting point and ending point , we can represent the vector symbolically using .    Draw the vectors below with given starting and ending points:  (a) and   (b) and    What can we say about the vectors and ?     Moving from to we travel 3 units in the direction and 2 units in the direction. Moving from to we travel units in the direction and units in the direction. Since the two arrows describe the same displacement, they have the same magnitude and the same direction; the only difference between them is where they start.     Equivalent Vectors  Vectors are said to be equivalent vectors if they have the same magnitude and direction (independent of their starting and ending points).    Vectors in Standard Position   If the starting point of a vector is the origin (the point ), we say the vector is in standard position.  Given a vector in standard position, if its ending point is ( ), we write down the vector as     and we say that is the -component of and is the -component of .    "
+},
+{
+  "id": "sec-ch01-01-Vectors2DIntroduction-2",
+  "level": "2",
+  "url": "sec-ch01-01-Vectors2DIntroduction.html#sec-ch01-01-Vectors2DIntroduction-2",
+  "type": "Definition",
+  "number": "1.1.1",
+  "title": "Vectors.",
+  "body": " Vectors   A vector is an object defined by     its magnitude (length)    and its direction.     They are geometrically represented by rays\/arrows and symbolically with a lowercase letter with an arrow above it or in bold face (such as or or , etc.).   "
+},
+{
+  "id": "sec-ch01-01-Vectors2DIntroduction-5",
+  "level": "2",
+  "url": "sec-ch01-01-Vectors2DIntroduction.html#sec-ch01-01-Vectors2DIntroduction-5",
+  "type": "Example",
+  "number": "1.1.2",
+  "title": "",
+  "body": "  Draw the vectors below with given starting and ending points:  (a) and   (b) and    What can we say about the vectors and ?     Moving from to we travel 3 units in the direction and 2 units in the direction. Moving from to we travel units in the direction and units in the direction. Since the two arrows describe the same displacement, they have the same magnitude and the same direction; the only difference between them is where they start.   "
+},
+{
+  "id": "sec-ch01-01-Vectors2DIntroduction-6",
+  "level": "2",
+  "url": "sec-ch01-01-Vectors2DIntroduction.html#sec-ch01-01-Vectors2DIntroduction-6",
+  "type": "Remark",
+  "number": "1.1.3",
+  "title": "Equivalent Vectors.",
+  "body": " Equivalent Vectors  Vectors are said to be equivalent vectors if they have the same magnitude and direction (independent of their starting and ending points).  "
+},
+{
+  "id": "sec-ch01-01-Vectors2DIntroduction-7",
+  "level": "2",
+  "url": "sec-ch01-01-Vectors2DIntroduction.html#sec-ch01-01-Vectors2DIntroduction-7",
+  "type": "Definition",
+  "number": "1.1.4",
+  "title": "Vectors in Standard Position.",
+  "body": " Vectors in Standard Position   If the starting point of a vector is the origin (the point ), we say the vector is in standard position.  Given a vector in standard position, if its ending point is ( ), we write down the vector as     and we say that is the -component of and is the -component of .   "
 },
 {
   "id": "backmatter-2",
